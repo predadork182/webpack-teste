4170f5d40de6e9a3127c75f724b3eb7b6f935982
marcos  tavares <marcos.vinicius@itarget.com.br>
2024-02-10 15:00:02 -0300

    Configuração de webserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
 const path = require('path'); // commonsjs
 
 module.exports = {
-  entry: './src/index.js',
-  output: { // gera o arquivo dentro de dist
+//   entry: './src/index.js',
+    entry: ["babel-polyfill", "./src/index.js"],
+    output: { // gera o arquivo dentro de dist
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-  },
-  module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ['@babel/preset-env'] 
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env'] 
+                }
+                }
             }
-          }
-        }
-      ]
-    }
-  }
+        ]
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 9000
+    },
+}
