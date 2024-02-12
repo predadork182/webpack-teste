10c830aa4b8a2206c7566093c538b9e0ba790b25
marcos  tavares <marcos.vinicius@itarget.com.br>
2024-02-12 09:38:04 -0300

    Adicionando novos arquivos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // criar gráfico de dependência
 import {} from './instancia-cliente';
+import style from  './assets/css/styles.css';
+import scss from './assets/scss/styles.scss'
 
 const es = "ES2015-const"
 console.log(es)
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,25 @@ module.exports = {
                     presets: ['@babel/preset-env'] 
                 }
                 }
-            }
+            },
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         { loader: 'style-loader' },
+            //         { loader: 'css-loader'},
+            //     ],
+            // },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
         ]
     },
     devServer: {
